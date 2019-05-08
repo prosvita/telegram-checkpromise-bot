9378abc440b5ef803860c5a3006a3ec93fe3616f
Pavlo Bashynskiy <levonet@gmail.com>
2019-05-08 16:11:29 +0300

    Новини не мають повторюватись при рестарті

--- configs/production/node.js
+++ configs/production/node.js
@@ -3,6 +3,6 @@ module.exports = {
     tgToken: process.env.TG_TOKEN || '000000000:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     defaultLanguage: process.env.DEFAULT_LANGUAGE || 'uk',
     dataUrl: process.env.DATA_JSON || 'https://www.checkpromise.info/assets/data/data.json',
-    schedule: process.env.SCHEDULE || '0 11 * * *',
+    schedule: process.env.SCHEDULE || '0 10 * * *',
     logLevel: process.env.LOG_LEVEL || 'info'
 }
